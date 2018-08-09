11245f98c673ed9ca064e23bd5dd1a6c5c6b115f
MACBOOK PRP <macbookprp@macbooks-macbook-pro.local>
2018-08-09 21:11:19 +0530

    Initial Commit

---
+++ TableviewTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableviewTask
+//
+//  Created by MACBOOK PRP on 09/08/18.
+//  Copyright © 2018 Bharath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
